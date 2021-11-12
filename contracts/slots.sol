c7900efa9fbcc7e9243f82d68b3b7bdf0ab644b1
thetrees1529 <54856258+thetrees1529@users.noreply.github.com>
2021-11-12 19:43:34 +0000

    terminology

--- contracts/slots.sol
+++ contracts/slots.sol
@@ -82,20 +82,20 @@ contract Slots is Ownable, Random {
 
 
     //previous results
-    function getHistoricalResultsLength(address _for) public view returns(uint) {
-        return _getHistoricalResultsLength(_for);
+    function getHistoricalResultsLengthOfPlayer(address _player) public view returns(uint) {
+        return _getHistoricalResultsLength(_player);
     }
 
-    function getHistoricalResultOfPlayerByIndex(address _for, uint _index) public view returns(string[3] memory) {
-        return _getHistoricalResult(_for, _index);
+    function getHistoricalResultOfPlayerByIndex(address _player, uint _index) public view returns(string[3] memory) {
+        return _getHistoricalResult(_player, _index);
     }
 
-    function getHistoricalWinningsLength(address _for) public view returns(uint) {
-        return _getHistoricalResultsLength(_for);
+    function getHistoricalWinningsLengthOfPlayer(address _player) public view returns(uint) {
+        return _getHistoricalResultsLength(_player);
     }
 
-    function getHistoricalWinningOfPlayerbyIndex(address _for, uint _index) public view returns(uint) {
-        return _getHistoricalWinning(_for, _index);
+    function getHistoricalWinningOfPlayerbyIndex(address _player, uint _index) public view returns(uint) {
+        return _getHistoricalWinning(_player, _index);
     }
 
 
@@ -187,124 +187,124 @@ contract Slots is Ownable, Random {
 
 
     
-    function _getHistoricalResults(address _for) private view returns(string[3][] storage) {
-        return _historicalResults[_for];
+    function _getHistoricalResults(address _player) private view returns(string[3][] storage) {
+        return _historicalResults[_player];
     } 
 
-    function _getHistoricalResultsLength(address _for) private view returns(uint) {
-        return _getHistoricalResults(_for).length;
+    function _getHistoricalResultsLength(address _player) private view returns(uint) {
+        return _getHistoricalResults(_player).length;
     }
 
-    function _getHistoricalResult(address _for, uint _index) private view returns(string[3] storage) {
-        return _getHistoricalResults(_for)[_index];
+    function _getHistoricalResult(address _player, uint _index) private view returns(string[3] storage) {
+        return _getHistoricalResults(_player)[_index];
     }
 
-    function _getHistoricalWinnings(address _for) private view returns(uint[] storage) {
-        return _historicalWinnings[_for];
+    function _getHistoricalWinnings(address _player) private view returns(uint[] storage) {
+        return _historicalWinnings[_player];
     }
 
-    function _getHistoricalWinningsLength(address _for) private view returns(uint) {
-        return _getHistoricalWinnings(_for).length;
+    function _getHistoricalWinningsLength(address _player) private view returns(uint) {
+        return _getHistoricalWinnings(_player).length;
     }
 
-    function _getHistoricalWinning(address _for, uint _index) private view returns(uint) {
-        return _getHistoricalWinnings(_for)[_index];
+    function _getHistoricalWinning(address _player, uint _index) private view returns(uint) {
+        return _getHistoricalWinnings(_player)[_index];
     }
 
 
 
-    function _getHeld(address _for) private view returns(bool[3] storage) {
-        return _held[_for];
+    function _getHeld(address _player) private view returns(bool[3] storage) {
+        return _held[_player];
     }
 
-    function _getPositions(address _for) private view returns(uint[3] storage) {
-        return _positions[_for];
+    function _getPositions(address _player) private view returns(uint[3] storage) {
+        return _positions[_player];
     }
 
     function _getReels() private view returns(string[][3] storage) {
         return _reels;
     }
 
-    function _getBank(address _for) private view returns(uint) {
-        return _bank[_for];
+    function _getBank(address _player) private view returns(uint) {
+        return _bank[_player];
     }
 
-    function _getCredits(address _for) private view returns(uint) {
-        return _credits[_for];
+    function _getCredits(address _player) private view returns(uint) {
+        return _credits[_player];
     }
     
 
 
-    function _resetHeld(address _for) private {
-        bool[3] storage held = _getHeld(_for);
+    function _resetHeld(address _player) private {
+        bool[3] storage held = _getHeld(_player);
         for(uint i; i < 3; i ++) {
             held[i] = false;
         }
     }
 
-    function _clearHolds(address _for) private {
-        _holds[_for] = 0;
+    function _clearHolds(address _player) private {
+        _holds[_player] = 0;
     }
 
-    function _clearNudges(address _for) private {
-        _nudges[_for] = 0;
+    function _clearNudges(address _player) private {
+        _nudges[_player] = 0;
     }
 
-    function _addToHolds(address _for, uint _value) private {
-        _holds[_for] += _value;
+    function _addToHolds(address _player, uint _value) private {
+        _holds[_player] += _value;
     }
 
-    function _addToNudges(address _for, uint _value) private {
-        _nudges[_for] += _value;
+    function _addToNudges(address _player, uint _value) private {
+        _nudges[_player] += _value;
     }
 
-    function _decrementHolds(address _for) private {
-        require(_holds[_for] > 0, "You have ran out of holds.");
-        _holds[_for] --;
+    function _decrementHolds(address _player) private {
+        require(_holds[_player] > 0, "You have ran out of holds.");
+        _holds[_player] --;
     }
 
-    function _decrementNudges(address _for) private {
-        require(_nudges[_for] > 0, "You have ran out of nudges.");
-        _nudges[_for] --;
+    function _decrementNudges(address _player) private {
+        require(_nudges[_player] > 0, "You have ran out of nudges.");
+        _nudges[_player] --;
     }
 
-    function _takeFromCredits(address _for, uint _value) private {
-        require(_credits[_for] >= _value, "You have ran out of credits.");
-        _credits[_for] -= _value;
+    function _takeFromCredits(address _player, uint _value) private {
+        require(_credits[_player] >= _value, "You have ran out of credits.");
+        _credits[_player] -= _value;
     }
 
-    function _addToCredits(address _for, uint _value) private {
-        _credits[_for] += _value;
-        emit Deposit(_for, _value);
+    function _addToCredits(address _player, uint _value) private {
+        _credits[_player] += _value;
+        emit Deposit(_player, _value);
     }
 
-    function _takeFromBank(address _for, uint _value) private {
-        require(_bank[_for] >= _value, "Your bank balance is too low to withdraw this amount.");
-        _bank[_for] -= _value;
-        emit Withdraw(_for, _value);
+    function _takeFromBank(address _player, uint _value) private {
+        require(_bank[_player] >= _value, "Your bank balance is too low to withdraw this amount.");
+        _bank[_player] -= _value;
+        emit Withdraw(_player, _value);
     }
 
-    function _addToBank(address _for, uint _value) private {
-        _bank[_for] += _value;
+    function _addToBank(address _player, uint _value) private {
+        _bank[_player] += _value;
     }
 
-    function _payForSpin(address _for) private {
-        _takeFromCredits(_for, _creditPrice);
+    function _payForSpin(address _player) private {
+        _takeFromCredits(_player, _creditPrice);
     }
 
-    function _awardHolds(address _for) private {
+    function _awardHolds(address _player) private {
         uint chance = _randomNumber(100);
         if(chance < _holdsChance) {
             uint holds = _randomNumber(_holdsMax - _holdsMin) + _holdsMin;
-            _addToHolds(_for, holds);
+            _addToHolds(_player, holds);
         }
     }
 
-    function _awardNudges(address _for) private {
+    function _awardNudges(address _player) private {
         uint chance = _randomNumber(100);
         if(chance < _nudgesChance) {
             uint nudges = _randomNumber(_nudgesMax - _nudgesMin) + _nudgesMin;
-            _addToNudges(_for, nudges);
+            _addToNudges(_player, nudges);
         }
     }
 
@@ -314,46 +314,46 @@ contract Slots is Ownable, Random {
     
 
 
-    function _spin(address _for) private {
+    function _spin(address _player) private {
         string[][3] storage reels = _getReels();
-        uint[3] storage positions = _getPositions(_for);
-        bool[3] storage held = _getHeld(_for);
+        uint[3] storage positions = _getPositions(_player);
+        bool[3] storage held = _getHeld(_player);
         for(uint i; i < 3; i ++) {
             if(!held[i]) {
                 uint pos = positions[i];
                 positions[i] = (pos + 1) % reels[i].length;
             }
         }
-        emit Spun(_for);
+        emit Spun(_player);
     }
 
 
 
-    function _nudge(address _for, uint _index) private {
+    function _nudge(address _player, uint _index) private {
         string[][3] storage reels = _getReels();
-        uint[3] storage positions = _getPositions(_for);
-        bool[3] storage held = _getHeld(_for);
+        uint[3] storage positions = _getPositions(_player);
+        bool[3] storage held = _getHeld(_player);
         require(!held[_index], "Cannot nudge a held position.");
         uint pos = positions[_index];
         positions[_index] = (pos + 1) % reels[_index].length;
-        _decrementNudges(_for);
-        _payout(_for);
+        _decrementNudges(_player);
+        _payout(_player);
     }
 
 
 
-    function _hold(address _for, uint _index) private {
-        bool[3] storage held = _getHeld(_for);
+    function _hold(address _player, uint _index) private {
+        bool[3] storage held = _getHeld(_player);
         require(!held[_index], "Already held this position.");
         held[_index] = true;
-        _decrementHolds(_for);
+        _decrementHolds(_player);
     }
 
 
 
-    function _payout(address _for) private {
+    function _payout(address _player) private {
         string[][3] storage reels = _getReels();
-        uint[3] storage positions = _getPositions(_for);
+        uint[3] storage positions = _getPositions(_player);
         string[3] memory result = [
             reels[0][positions[0]], 
             reels[1][positions[1]],
@@ -365,23 +365,23 @@ contract Slots is Ownable, Random {
             string[3] memory payLine = payLineData.payLine;
             uint payout = payLineData.payout;
             if(keccak256(abi.encode(payLine)) == keccak256(abi.encode(result))) {
-                XYA.transfer(_for, payout);
-                emit Won(_for, payout);
+                XYA.transfer(_player, payout);
+                emit Won(_player, payout);
             }
             winnings += payout;
         }
-        _logHistoricalResults(_for, result);
-        _logHistoricalWinnings(_for, winnings);
+        _logHistoricalResults(_player, result);
+        _logHistoricalWinnings(_player, winnings);
     }
 
 
 
-    function _logHistoricalResults(address _for, string[3] memory _result) private {
-        string[3][] storage historicalResults = _getHistoricalResults(_for);
+    function _logHistoricalResults(address _player, string[3] memory _result) private {
+        string[3][] storage historicalResults = _getHistoricalResults(_player);
         historicalResults.push(_result);
     }
-    function _logHistoricalWinnings(address _for, uint _winnings) private {
-        uint[] storage historicalWinnings = _getHistoricalWinnings(_for);
+    function _logHistoricalWinnings(address _player, uint _winnings) private {
+        uint[] storage historicalWinnings = _getHistoricalWinnings(_player);
         historicalWinnings.push(_winnings);
     }
 }
